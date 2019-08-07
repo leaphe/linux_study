9a0e0c9ee451fc816c95239c8d2ea3817edd2858
leaphe <13918825181@qq.com>
2019-08-07 14:11:10 +0800

    Davis

--- hello.sh
+++ hello.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #The first program
-echo -e "\033[36m天下掉下个林妹妹\033[0m"
-.
-#字体颜色：30m-37m 黑、红、绿、黄、蓝、紫、青、白
+
+#/Users/hexizan/Documents/xproject/TSTACK/广东农行TStack\ POC测试报告模板.docx #字体颜色：30m-37m 黑、红、绿、黄、蓝、紫、青、白
+
 str="天下掉下个林妹妹"
 echo -e "\033[30m ${str}\033[0m"      ## 黑色字体
 echo -e "\033[31m ${str}\033[0m"      ## 红色
@@ -11,6 +11,53 @@ echo -e "\033[33m ${str}\033[0m"      ## 黄色
 echo -e "\033[34m ${str}\033[0m"      ## 蓝色
 echo -e "\033[35m ${str}\033[0m"      ## 紫色
 echo -e "\033[36m ${str}\033[0m"      ## 青色
-echo -e "\033[37m ${str}\033[0m"      ## 白色
+echo -e "\033[37m ${str}\033[0m"    d  ## 白色
 
 #chmod 755 hello.sh
+#./hello.sh
+
+#alias vi='vim'
+#vi /root/.bashrc
+#scurce /root/.bashrc
+
+#echo $PATH
+#ping www.baidu.com
+
+
+#cat ~/.bash_history
+
+#histtory -w
+
+#vi /etc/profile
+#dev/stdin
+#dev/stdout
+#dev/stderr
+
+#ls &>/dev/null
+#ls &>>test.log = ls >>test.log 2>&1 
+#cat xdy>>access.log 2>error.log
+
+#wc
+#ctrol+d
+#wc test.log
+
+#date;sleep 5;date;
+#ls  && echo yes
+#ls  || echo yes
+
+#ls  && echo yes||echo no
+
+#echo test|more
+#ls |more
+#netstat -an
+#netstat -an |grep ESTABLISHED
+
+#aa=10
+#echo $aa
+
+
+#bb=$(ls)
+#echo $bb
+
+
+
